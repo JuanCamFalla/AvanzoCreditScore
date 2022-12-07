496df6c468c0108ef1d1c0973f85c2a83699d8b8
Juan Camilo Falla <j.fallag@uniandes.edu.co>
2022-12-07 17:29:53 -0500

    commit commitoso

--- monitoring/deudores/models.py
+++ monitoring/deudores/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+class Deudor(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    cedula = models.CharField(max_length=50)
+    telefono = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    version = models.FloatField()
+
+    def __str__(self):
+        return self.nombre, self.apellido
+
+
 # Create your models here.
--- monitoring/deudores/views.py
+++ monitoring/deudores/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Deudor
+
+
+def DeudorList(request):
+    queryset = Deudor.objects.all()
+    context = list(queryset.values('id'))
+    return JsonResponse(context, safe=False)
+
 
 # Create your views here.
